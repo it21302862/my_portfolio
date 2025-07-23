48e0882cee7f592bf42deb88fba29c865b5f67d2
IT21302862 <sanayasamadhi@gmail.com>
2025-07-24 01:04:52 +0530

    Change the CV Resume Link

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/it21302862",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/19_8h0xlfzua_f_t31eSWcGJUm8g0vK8S/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sanaya-samadhi/",
   twitter: "https://x.com/SamadhiSanaya",
   insta: "https://www.instagram.com/sanayasamadhi/",
