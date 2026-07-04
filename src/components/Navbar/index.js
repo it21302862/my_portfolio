3eb37803e23e0a5ea600bf2a293df7db270e57ac
IT21302862 <sanayasamadhi@gmail.com>
2026-07-04 12:24:17 +0530

    B0006: Updated for production level

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,9 +12,8 @@ const Navbar = () => {
     <Nav>
       <NavbarContainer>
         <NavLogo to='/'>
-          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
-          </a>
+          <DiCssdeck size="3rem" />
+          <Span>Portfolio</Span>
         </NavLogo>
         <MobileIcon>
           <FaBars onClick={() => {
